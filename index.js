106ca5eddc01b75e48f6aec2b93f3fae470e4c0e
Haraga Ákos <hakos@gmail.hu>
2023-10-23 17:27:26 +0200

    Második Commit: Kezdőoldal, Termékek menük stílus formázása, képek beillesztése.

---
+++ index.js
@@ -0,0 +1,9 @@
+/*let searchNavbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    searchNavbar.classList.toggle('active');
+}*/
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() =>{
+    search.classList.toggle('active');
+}
